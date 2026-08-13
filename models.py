7a73110d40a7ef0104d924278ae99efa195ab6ad
chengzhi-c <chengzhicc2022@163.com>
2026-08-13 21:09:49 +0800

    release: bump to 1.1.1

--- models.py
+++ models.py
@@ -22,7 +22,7 @@ from typing import Any, Protocol
 from astrbot.api import logger
 
 PLUGIN_ID = "astrbot_plugin_self_initiated_reply"
-PLUGIN_VERSION = "1.1.0"
+PLUGIN_VERSION = "1.1.1"
 COMMAND_HANDLED_KEY = f"{PLUGIN_ID}:command_handled"
 STATE_VERSION = 4
 
